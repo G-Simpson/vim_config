1becf2cb2c76ce950c3895937d540424a6cc256f
Gerard <gardesimpson@gmail.com>
2024-10-04 09:38:20 +1000

    adding surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,6 +30,16 @@ return {
      "slim-template/vim-slim",
       lazy = false,
    },
+   {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+   }
   --
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
